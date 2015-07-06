271e3f74506a47ad9700e06e8b55217ba3850940
makinoshi <masa.kinoshita02@gmail.com>
2015-07-06 23:06:56 +0900

    change reactify to babelify to use ES6.

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,12 @@ var gulp = require('gulp'),
     browserify = require('browserify'),
     source = require('vinyl-source-stream'),
     buffer = require('vinyl-buffer'),
-    reactify = require('reactify'),
+    babelify = require('babelify'), 
     plumber = require('gulp-plumber'),
     uglify = require('gulp-uglify'),
     browserSync = require('browser-sync'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps');
 
 // local server
 gulp.task('server', function() {
@@ -20,35 +21,23 @@ gulp.task('server', function() {
   });
 });
 
-// html task
-gulp.task('html', function() {
-  gulp.src('./*.html')
-      .pipe(browserSync.reload());
-});
-
-// css task
-gulp.task('css', function() {
-  gulp.src('./css/**/*.css')
-      .pipe(browserSync.reload());
-});
-
-// js task
-gulp.task('js', function() {
-  gulp.src('./dist/**/*.js')
-      .pipe(browserSync.reload());
+// reload browser
+gulp.task('reload', function() {
+  browserSync.reload();
 });
 
 // compile react .jsx files
 gulp.task('react', function(){
-  browserify({
-    entries: ['./js/main.jsx'],
-    transform: [reactify]
-  }).bundle()
+  browserify('./js/main.jsx', { debug: true })
+      .transform(babelify)
+      .bundle()
       .on("error", function (err) { console.log("Error : " + err.message); })
       .pipe(source('main.js'))
       .pipe(plumber())
       .pipe(buffer())
-      // .pipe(uglify())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(uglify())
+      .pipe(sourcemaps.write('./'))
       .pipe(gulp.dest('./dist/'));
 });
 
@@ -62,9 +51,8 @@ gulp.task('sass', function() {
 
 // file watch and run tasks
 gulp.task('watch', function() {
-  gulp.watch(['./*.html'], ['html']);
-  gulp.watch(['./css/**/*.css'], ['css']);
-  gulp.watch(['./dist/**/*.js'], ['js']);
+  gulp.watch(['./*.html', './css/**/*.css', './dist/**/*.js'], ['reload']);
+  // gulp.watch(['./css/**/*.css'], ['Css']);
   gulp.watch(['./scss/**/*.scss'], ['sass']);
   gulp.watch(['./js/*.js', './js/*.jsx'], ['react']);
 });
